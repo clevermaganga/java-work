577fba21313c2c609083802dc31a60afbf958ea4
clevermaganga <clevermaganga9@gmail.com>
2026-04-24 16:25:13 +0300

    Add files via upload

---
+++ Main.java
@@ -0,0 +1,11 @@
+class Test {
+    public static void main(String[] args) {
+        int marks = 75;
+
+        if (marks >= 50) {
+            System.out.println("Pass");
+        } else {
+            System.out.println("Fail");
+        }
+    }
+}
